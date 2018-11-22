25e316b2b8c99242bdb718493aea0970372ec042
Sivaranjith Sivarasa <sivara1210@osloskolen.no>
2018-11-22 14:00:32 +0100

    ting

--- javascript.js
+++ javascript.js
@@ -32,14 +32,29 @@ function menu() {
 //på endring av første select
 forsteSelect.addEventListener('change', changeFirst, false)
 andreSelect.addEventListener('change', changeAndre, false)
+let differanseTall = 0
 
 function changeFirst(evt) {
 	andreSelect.style.display = 'block'
+	andreSelect.innerHTML = ''
+	andreSelect.innerHTML += '<option disabled selected value> -- Velg årstall -- </option>'
+
+	//setter inn tall i select
+	const forsteArstall = Number(evt.target.value)
+	for (let i = forsteArstall + 1; i <= 2007; i++) {
+		andreSelect.innerHTML += `<option value="${i}">${i}</option>`
+	}
+
+	//regne differanse
+	console.log(differanse())
 }
 
-function changeAndre(evt) {
-	const forsteArstall = forsteSelect.value
-	evt.target.innerHTML += '<option disabled selected value> -- Velg årstall -- </option>'
+function changeAndre() {
 
+	//regne differanse
+	differanse()
+}
 
+function differanse() {
+	return data[andreSelect.value].promillekjøring - data[forsteSelect.value].promillekjøring
 }
