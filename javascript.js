4218b7d89d459e0035279d40512d472101e8c8b5
Andreas Houg <andreh1807@osloskolen.no>
2018-11-22 13:14:44 +0100

    kukk

---
+++ javascript.js
@@ -0,0 +1,26 @@
+let data = {
+	2002: {
+		promillekjøring: 9631,
+		ulovligHastighet: 9863
+	},
+	2003: {
+		promillekjøring: 8593,
+		ulovligHastighet: 12217
+	},
+	2004: {
+		promillekjøring: 8363,
+		ulovligHastighet: 14920
+	},
+	2005: {
+		promillekjøring: 8128,
+		ulovligHastighet: 14929
+	},
+	2006: {
+		promillekjøring: 8514,
+		ulovligHastighet: 15425
+	},
+	2007: {
+		promillekjøring: 8534,
+		ulovligHastighet: 18010
+	}
+};
